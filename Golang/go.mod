505d79f2a2f8c063658e0b13abf1fcc79d74e4b4
Vitor Negromonte <vitor.negromonte@ufpe.br>
2023-10-10 14:44:04 -0300

    BinarySearch in Golang

---
+++ Golang/go.mod
@@ -0,0 +1,3 @@
+module grokking-algorithms
+
+go 1.21.1
---
+++ Golang/src/01-BinarySearch.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func binarySearch(list []int, target int) int {
+	low := 0
+	high := len(list) - 1
+
+	for low <= high {
+		mid := (low + high) / 2
+
+		if list[mid] == target {
+			return mid
+		}
+		if list[mid] < target {
+			low = mid + 1
+		} else {
+			high = mid - 1
+		}
+	}
+
+	return -1
+}
+
+func main() {
+	sample_list := []int{1, 3, 5, 7, 9}
+
+	fmt.Println(binarySearch(sample_list, 3))
+	fmt.Println(binarySearch(sample_list, -1))
+}
